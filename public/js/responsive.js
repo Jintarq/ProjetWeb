c59b265cf79a5298e05210ea11faaa561fd2ca93
Jintarq <axelboyerllb@gmail.com>
2022-11-28 08:26:22 +0100

    final commit 2.0

--- public/js/responsive.js
+++ public/js/responsive.js
@@ -1,8 +1,15 @@
 // Responsive javascript part
 
+//The issue there is that when you click on a nav link that links an element of the same page, it doesn't toggle "open" class
+const navLink = document.querySelector("#nav-link");
 const menuResp = document.querySelector("#menu-responsive");
 const body = document.querySelector("body");
-
+console.log(navLink);
 menuResp.addEventListener("click", () => {
+  body.classList.toggle("open");
+});
+
+// So we need to toggle the click too when we click on it
+navLink.addEventListener("click", () => {
   body.classList.toggle("open");
 });
